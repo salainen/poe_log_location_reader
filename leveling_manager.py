91bf564d17b58cf0495f465f1ce4cab6056bc1fa
salainen <vuonojenmustaturska@hotmail.com>
2021-01-10 19:37:26 +0200

    revamped the ui

--- leveling_manager.py
+++ leveling_manager.py
@@ -13,6 +13,9 @@ leveling_text = dir_path + "/leveling_setup.txt"
 global level_data
 global stop_time
 stop_time = False
+global time_at_stop
+
+time_at_stop = ""
 
 def read_instructions(line):
 
@@ -102,39 +105,41 @@ def add_row(text_to_add):
     items = []
     var = IntVar()
 
-    # create checkmark for deletion purposes:
-    check_mark = Checkbutton(root, variable = var)
-    check_mark.val = var
-    items.append(check_mark)
-    check_mark.grid(row = i, column = 0)
-
-    # create location_data label:
-    location_label = Entry(root, width=30)
-    location_label.insert(END, location_data)
-    items.append(location_label)
-    location_label.grid(row=i, column=1)
+    # print(i)
+    to_be_deleted = i-2
+    delete_entry = tkinter.Button(root , text = 'del', command = lambda: delete_row(to_be_deleted))
+    delete_entry.grid(sticky="E,W", row=i, column=0)
+    rows.append(delete_entry)
 
     # create notes label:
-    notes_label = Entry(root, width=70)
+    notes_label = Entry(root, width=50)
     notes_label.insert(END, notes_data)
     items.append(notes_label)
-    notes_label.grid(row=i, column=2)
+    notes_label.grid(sticky="E,W", row=i, column=1)
 
+    # add all added items to rows -list:
     rows.append(items)
+    row_index.append(i)
 
-def delete_row():
 
-    ''' deletes row(s) from the GUI
+def delete_row(index_to_delete):
+
+    ''' deletes a row from the GUI
+
+    When del button is pressed, the del button
+    and the text label in the same row will be
+    deleted from the GUI.
 
     '''
+    # calculate the button and text label positions
+    # in the rows -list
+    delete_this = index_to_delete*2-1
+    delete_this2 = index_to_delete*2-2
 
-    for rowno, row in reversed(list(enumerate(rows))):
-    
-        if row[0].val.get() == 1:
-    
-            for i in row:
-                i.destroy()
-            rows.pop(rowno)
+    # delete the objects from GUI:
+    for x in rows[delete_this]:
+            x.destroy()
+    rows[delete_this2].destroy()
 
 def start_timer():
 
@@ -143,14 +148,28 @@ def start_timer():
     Starts the timer via starting a new thread timer_thread()
     Also sets the global flag stop_time to False
     '''
-
+    
     global stop_time
+    global time_at_stop
     stop_time = False
     timer = threading.Thread(target=timer_thread, args=(1,))
     timer.start()
 
 def stop_timer():
 
+    ''' Stops the timer
+
+    '''
+
+    for x in timer_store:
+        x.destroy()
+
+    # Stop start clock button
+    timer_start_button = tkinter.Button(root , text = 'Start timer', command = start_timer)
+    timer_start_button.grid(sticky="E,W", row=0, column=0)
+    timer_store.append(timer_start_button)
+    
+    
     global stop_time
     stop_time = True
 
@@ -161,31 +180,73 @@ def timer_thread(time_arg):
     As long as global variable stop_time is set to False, 
     the timer will run. It'll update timer_text label 
     every 10 milliseconds.
-    '''
 
+    If timer has been previously started and then stopped
+    the counter will resume from time_at_stop value.
+    '''
+    for x in timer_store:
+        x.destroy()
+
+    # Stop start clock button
+    timer_stop_button = tkinter.Button(root , text = 'Stop timer', command = stop_timer)
+    timer_stop_button.grid(sticky="E,W", row=0, column=0)
+    timer_store.append(timer_stop_button)
+        
     # creates the timer label in the main UI
     timer_text = StringVar()
-    timer_label = Entry(root, textvariable = timer_text, state = 'readonly')
-    timer_text.set('00:00:00')
-    timer_label.grid(row=0, column=2)
+    timer_label = Entry(root, textvariable = timer_text, state = 'readonly', width=10)
+    #timer_text.set('00:00:00')
+    timer_label.config(font=("Courier", 30))
+    timer_label.grid(sticky="E,W", row=0, column=1)
 
     global stop_time
+    global time_at_stop
 
     # time when the thread was called:
+
     start_time = datetime.datetime.now()
+
+    # if time_at_stop is set, use it
+    if len(str(time_at_stop)) >= 1:
+        start_time = datetime.datetime.now() - time_at_stop
+
 
     # thread loop. loop until stop_time global variable is set
     # to True:
     while not stop_time:
         time.sleep(0.01)
         current_time = datetime.datetime.now()
-        
+
         # calculate time from thread start vs. current time.
         time_difference = current_time - start_time
         
         # set the label text:
         timer_text.set(str(time_difference)[:-3])
-        
+        time_at_stop = time_difference
+
+def border_remove():
+
+    ''' removes window borders
+
+    '''
+    for x in borders_store:
+        x.destroy()
+    root.overrideredirect(1)
+    add_borders = tkinter.Button(root , text = 'add borders', command = border_add)
+    add_borders.grid(sticky="E,W", row=1, column=0)
+    borders_store.append(add_borders)
+
+def border_add():
+
+    ''' re-adds window borders
+
+    '''
+    for x in borders_store:
+        x.destroy()
+    root.overrideredirect(0)
+    remove_borders = tkinter.Button(root , text = 'remove borders', command = border_remove)
+    remove_borders.grid(sticky="E,W", row=1, column=0)
+    borders_store.append(remove_borders)
 
 def init_window():
 
@@ -193,34 +254,15 @@ def init_window():
 
     '''
 
-    # Add delete row button
-    delete_button = tkinter.Button(root , text = 'Delete selected Row', command = delete_row)
-    delete_button.grid(row =0, column=0)
-
     # Add start clock button
-    timer_button = tkinter.Button(root , text = 'Start timer', command = start_timer)
-    timer_button.grid(row=0, column=1)
+    timer_start_button = tkinter.Button(root , text = 'Start timer', command = start_timer)
+    timer_start_button.grid(sticky="E,W", row=0, column=0)
+    timer_store.append(timer_start_button)
 
-    # Stop start clock button
-    timer_stop_button = tkinter.Button(root , text = 'Stop timer', command = stop_timer)
-    timer_stop_button.grid(row=0, column=3)
-
-
-    # Add header row
-    select_text = StringVar()
-    entry_1 = Entry(root, textvariable = select_text, state = 'readonly')
-    select_text.set('Select')
-    entry_1.grid(row = 1, column = 0 )
-
-    info_labels = StringVar()
-    entry_2 = Entry(root, textvariable = info_labels, state = 'readonly', width=30)
-    info_labels.set('Place')
-    entry_2.grid(row = 1, column = 1 )
-
-    info_labels = StringVar()
-    entry_2 = Entry(root, textvariable = info_labels, state = 'readonly', width=70)
-    info_labels.set('Notes')
-    entry_2.grid(row = 1, column = 2 )
+    # add remove borders button
+    remove_borders = tkinter.Button(root , text = 'remove borders', command = border_remove)
+    remove_borders.grid(sticky="E,W", row=1, column=0)
+    borders_store.append(remove_borders)
 
 # main stuff under this line:
 
@@ -233,7 +275,11 @@ level_data = [x.strip() for x in level_data]
 # create the main window:
 i=2
 rows = []
+row_index = []
 root = Tk()
+
+borders_store = []
+timer_store = []
 
 init_window()
 
@@ -251,6 +297,7 @@ file_reader.start()
 
 # make sure our window stays on top:
 root.wm_attributes("-topmost", 1)
+# root.overrideredirect(1)
 
 # start main tkinter loop:
 mainloop()
